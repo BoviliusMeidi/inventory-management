078741b126cb2cc5e4b7c4904a3e0febad25e18b
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-14 23:15:05 +0700

    feat(lib/hooks): add use-pagination

---
+++ src/lib/hooks/use-pagination.ts
@@ -0,0 +1,19 @@
+"use client";
+
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
+export function usePagination() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const currentPage = Number(searchParams.get("page")) || 1;
+
+  const handlePageChange = (newPage: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", String(newPage));
+    router.push(`${pathname}?${params.toString()}`);
+  };
+
+  return { currentPage, handlePageChange };
+}
