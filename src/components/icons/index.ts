b5ff5aa50523de4845e3bfead4f18307fc8ef963
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-14 15:55:06 +0700

    refactor(components/icons): create index barrel file

---
+++ src/components/icons/index.ts
@@ -0,0 +1,19 @@
+export * from "./DashboardIcon";
+export * from "./InventoryIcon";
+export * from "./ReportsIcon";
+export * from "./SuppliersIcon";
+export * from "./OrdersIcon";
+export * from "./ManageStoreIcon";
+export * from "./SettingsIcon";
+export * from "./LogOutIcon";
+
+export * from "./SearchIcon";
+export * from "./NotificationIcon";
+
+export * from "./HamburgerIcon";
+export * from "./CloseIcon";
+
+export * from "./ProfileIcon";
+
+export * from "./EyeIcon";
+export * from "./EyeSlashIcon";
