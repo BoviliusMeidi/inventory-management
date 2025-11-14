d9f85210e8706791efc3fe9e31f8ee4faa270ac6
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-15 00:20:06 +0700

    refactor(page): update to use component/features/suppliers

--- src/app/(app)/suppliers/page.tsx
+++ src/app/(app)/suppliers/page.tsx
@@ -1,15 +1,13 @@
 "use client";
-import SupplierTable from "@/components/SupplierTable";
-import AddSupplier from "../../../components/AddSupplier";
+import SupplierTable from "@/components/features/suppliers/SupplierTable";
+import AddSupplier from "../../../components/features/suppliers/AddSupplier";
 
 export default function SuppliersPage() {
   return (
-      <div className="flex flex-col gap-3 mr-3">
+      <div className="flex flex-col gap-3">
         <div className="bg-white shadow-md p-4 rounded-md">
-          {/* Products -- Block 1 */}
           <div className="flex flex-row justify-between items-center gap-3">
             <h1 className="text-lg pb-2 tracking-wide">Suppliers</h1>
-            {/* Button -- Add , Filter, Download */}
             <div className="flex flex-row gap-4 tracking-wide">
               <AddSupplier />
             </div>
