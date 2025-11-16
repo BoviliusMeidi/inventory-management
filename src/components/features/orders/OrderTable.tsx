8593e2531994b5436d63fbe9f4ee3c3922fae182
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-16 16:43:40 +0700

    refactor(components/features): update to use OrderItemsModal

--- src/components/features/orders/OrderTable.tsx
+++ src/components/features/orders/OrderTable.tsx
@@ -5,11 +5,12 @@ import { getPaginatedOrders, Order } from "@/lib/actions/orders";
 import Pagination, { PAGE_SIZE } from "@/components/ui/Pagination";
 import { usePagination } from "@/lib/hooks/use-pagination";
 import OrderRow from "@/components/features/orders/OrderRow";
+import OrderItemsModal from "@/components/features/orders/OrderItemsModal";
 
 export default function OrderTable({
   selectedFilter,
   refreshKey,
-  onOrderChange
+  onOrderChange,
 }: {
   selectedFilter: string | null;
   refreshKey: number;
@@ -18,6 +19,9 @@ export default function OrderTable({
   const [orders, setOrders] = useState<Order[]>([]);
   const [totalPages, setTotalPages] = useState(1);
   const { currentPage, handlePageChange } = usePagination();
+  const [viewingOrderItems, setViewingOrderItems] = useState<Order | null>(
+    null
+  );
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,36 +41,49 @@ export default function OrderTable({
   }, [currentPage, selectedFilter, refreshKey]);
 
   return (
-    <div className="pt-2 overflow-hidden">
-      <div className="overflow-x-auto">
-        <table className="min-w-full bg-white text-left text-sm sm:text-base">
-          <thead>
-            <tr>
-              <th className="py-2 px-2 md:px-4">ID</th>
-              <th className="py-2 px-2 md:px-4">Supplier</th>
-              <th className="py-2 px-2 md:px-4 hidden md:table-cell">
-                Total Cost
-              </th>
-              <th className="py-2 px-2 md:px-4 hidden lg:table-cell">Items</th>
-              <th className="py-2 px-2 md:px-4 hidden md:table-cell">
-                Expected
-              </th>
-              <th className="py-2 px-2 md:px-4">Status</th>
-              <th className="py-2 px-4">Actions</th>
-            </tr>
-          </thead>
-          <tbody className="border-t border-gray-300">
-            {orders.map((order) => (
-              <OrderRow key={order.id} order={order} onOrderChange={onOrderChange}/>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
+    <>
+      <OrderItemsModal
+        order={viewingOrderItems}
+        onClose={() => setViewingOrderItems(null)}
       />
-    </div>
+      <div className="pt-2 overflow-hidden">
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white text-left text-sm sm:text-base">
+            <thead>
+              <tr>
+                <th className="py-2 px-2 md:px-4 hidden md:table-cell">ID</th>
+                <th className="py-2 px-2 md:px-4">Supplier</th>
+                <th className="py-2 px-2 md:px-4 hidden md:table-cell">
+                  Total Cost
+                </th>
+                <th className="py-2 px-2 md:px-4">
+                  Items
+                </th>
+                <th className="py-2 px-2 md:px-4 hidden md:table-cell">
+                  Expected
+                </th>
+                <th className="py-2 px-2 md:px-4">Status</th>
+                <th className="py-2 px-4">Actions</th>
+              </tr>
+            </thead>
+            <tbody className="border-t border-gray-300">
+              {orders.map((order) => (
+                <OrderRow
+                  key={order.id}
+                  order={order}
+                  onOrderChange={onOrderChange}
+                  onViewItems={() => setViewingOrderItems(order)}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </>
   );
 }
