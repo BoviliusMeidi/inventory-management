780ee706a29458a39f2961c26d213a7f74356dd0
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-30 20:51:19 +0700

    chore(next.config): add Supabase domain to image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // devIndicators:false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xrlacnmgrlzodrwjrmvi.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
